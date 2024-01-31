c41f52a03b96466a60aa40a97bd920ecfc42af39
SteGala <galaxx.ste@gmail.com>
2024-01-31 10:08:01 +0100

    update ploit

--- plot.py
+++ plot.py
@@ -2,29 +2,57 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    filenames = ["1_LGF_FIFO_0_split.csv", "2.csv", "prova2.csv"]
+    filenames = ["brute-force.csv", "0_POWER_FIFO_0_split.csv", "2_POWER_FIFO_0_split.csv", "4_POWER_FIFO_0_split.csv", "kubernetes.csv"]
     labels = {
-        "1_LGF_FIFO_0_split.csv": "Plebiscito-CPU",
-        "2.csv": "Plebiscito-POWER",
-        "prova2.csv": "Brute Force"
+        "brute-force.csv": "Brute Force",
+        "0_POWER_FIFO_0_split.csv": "Plebiscito-POWER-0",
+        "2_POWER_FIFO_0_split.csv": "Plebiscito-POWER-2",
+        "4_POWER_FIFO_0_split.csv": "Plebiscito-POWER-4",
+        "kubernetes.csv": "Kubernetes"
     }
     data = {}
+    
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    
 
     for filename in filenames:
         df = pd.read_csv(filename)
         df = df.filter(regex=(".*consumption"))
         
-        plt.plot([i for i in range(len(df))], list(df.sum(axis=1)), label=labels[filename])
-        
-    # data_df = pd.DataFrame(data)
-    # data_df.plot.line()
-    plt.legend()
-    plt.xlabel("Time")
-    plt.ylabel("Power Consumption")
-
-    plt.savefig("helllooooo")
+        ax1.plot([i for i in range(len(df))], list(df.sum(axis=1)), label=labels[filename])
+        data[labels[filename]] = list(df.sum(axis=1))
+    
+    max_len = max([len(v) for v in data.values()])  
+    plot_data = []
+    
+    for i in range(max_len):
+        d = []
+        for l in labels:
+            if len(data[labels[l]]) > i:
+                d.append(data[labels[l]][i])
+            else:
+                d.append(0)
+        plot_data.append(d)
+            
+    df = pd.DataFrame(plot_data, columns=list(labels.values()))
+    _ = df.boxplot(ax=ax2, rot=90)
+    
+    ax1.legend()
+    ax1.set_xlabel("Time")
+    ax1.set_ylabel("Power Consumption (W)")
+    #fig.xlabel("Time")
+    #fig.ylabel("Power Consumption")
+    plt.tight_layout()
+    fig.savefig("helllooooo")
 
 def plot_consumption(nodes):
+    distinct_node_type = []
+    for n in nodes:
+        if n.gpu_type not in distinct_node_type:
+            distinct_node_type.append(n.gpu_type)
+    
+    fig, axs = plt.subplots(len(distinct_node_type))
+        
     for n in nodes:
         cpu_power = []
         cpu_performance = []
@@ -39,7 +67,13 @@ def plot_consumption(nodes):
             cpu_efficiency.append(n.performance.compute_current_efficiency_cpu(i))
         
         x = [i for i in range(1, n.initial_cpu+1)]
-        plt.plot(x, cpu_power, label=f"{n.id}")
+        id = 0
+        for id2, t in enumerate(distinct_node_type):
+            if n.gpu_type == t:
+                id = id2
+                break
+            
+        axs[id].plot(x, cpu_power, label=f"{n.id}")
             
         # do the same for gpu
         for i in range(1, n.initial_gpu+1):
@@ -47,5 +81,5 @@ def plot_consumption(nodes):
             gpu_performance.append(n.performance.compute_current_performance_gpu(i))
             gpu_efficiency.append(n.performance.compute_current_efficiency_gpu(i))
 
-    plt.tight_layout()
-    plt.savefig('cpu_gpu_power_performance.png')
+    fig.tight_layout()
+    fig.savefig('node_power_consumption.png')
